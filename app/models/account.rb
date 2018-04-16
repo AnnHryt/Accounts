dc3fd87740047bcf4af47dc6fe5732ff37d87f15
AnnHryt <hrytsyuka@gmail.com>
2018-04-15 21:58:38 -0700

    First name, last name and ssn presence validation

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,5 @@
 class Account < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :ssn, presence: true
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe Account, type: :model do
   it "has a valid factory" do
     expect(account.save).to be_truthy  
   end
+  it "is not valid without a first name" do 
+    account.first_name = nil
+    expect(account).to_not be_valid
+    expect(account.errors[:first_name].any?).to be_truthy
+  end
+  it "is not valid without a last name" do 
+    account.last_name = nil
+    expect(account).to_not be_valid
+    expect(account.errors[:last_name].any?).to be_truthy
+  end
+  it "is not valid without a ssn" do 
+    account.ssn = nil
+    expect(account).to_not be_valid
+    expect(account.errors[:ssn].any?).to be_truthy
+  end
 end
